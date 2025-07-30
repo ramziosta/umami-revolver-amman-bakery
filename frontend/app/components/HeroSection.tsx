b461f41d1c770efb3449a2a93e7908ac22f6bb66
ramziosta <ramziosta@gmail.com>
2025-07-30 22:08:00 +0300

    edits to hero

--- frontend/app/components/HeroSection.tsx
+++ frontend/app/components/HeroSection.tsx
@@ -20,22 +20,22 @@ export const HeroSection = () => {
             </div>
 
             {/* Content */}
-            <div className="relative z-10 text-center px-4 max-w-3xl mx-auto flex flex-col justify-center items-center h-full sm:pt-[18vh] md:pt-0">
-                <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-9xl font-blanka text-white tracking-wide leading-snug mb-3">
-                    Baked to Break<br /> the Mold
+            <div className="relative z-10 text-center px-4 max-w-3xl mx-auto flex flex-col justify-center items-center h-full top-2 sm:top-8">
+                <h1 className="text-2xl sm:text-3xl md:text-6xl lg:text-8xl font-blanka text-white tracking-wide leading-snug ">
+                    Baked to Break the Mold
                 </h1>
 
-                <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl italic text-umami-berry font-lovan mb-4 sm:mb-6">
+                <p className="lg:text-6xl  md:text-4xl italic text-umami-berry font-lovan">
                     Flavor. Elevated.
                 </p>
 
-                <p className="text-base sm:text-lg md:text-xl text-umami-nube font-ppneuemontreal leading-relaxed max-w-xl mx-auto mt-2">
+                <p className="text-base text-smsm:text-sm md:text-xl lg:text-3xl text-umami-nube font-ppneuemontreal leading-relaxed max-w-xl mx-auto md:mt-6 lg:mt-6">
                     Not just baked. Engineered. Every crumb, crust, and layer crafted to surprise.
                     Even our brownie? A bold, edible statement.
                 </p>
 
                 {/* Scroll Indicator */}
-                <div className="mt-10 sm:mt-12 flex justify-center animate-bounce">
+                <div className="mt-6 sm:mt-1 flex justify-center animate-bounce">
                     <ArrowDown className="h-6 w-6 sm:h-8 sm:w-8 text-umami-nube opacity-80" />
                 </div>
             </div>
